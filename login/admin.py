375830978c854f48727f7a3cff242990a89c9619
Alberto Cesar <albertocesar.be@gmail.com>
2025-02-21 11:46:44 -0300

    realizando migrations

--- login/admin.py
+++ login/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from login.models import Usuario
 
 # Register your models here.
+
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email')
+
+admin.site.register(Usuario, UsuarioAdmin)
