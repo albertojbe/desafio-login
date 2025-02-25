7d8de467305a72d2a2d8cc5ff642864b9af756d6
Alberto Cesar <albertocesar.be@gmail.com>
2025-02-25 12:41:10 -0300

    refactor: refatorando tela e view de cadastro. A requisição para cadastro será feita via ajax e as validações serão feitas do lado do servidor

--- login/views.py
+++ login/views.py
@@ -1,24 +1,49 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib import messages
-from django.http import HttpResponse, HttpResponseRedirect
-from .models import Usuario, AccountManager
+from django.http import JsonResponse
+from .models import Usuario
+import json, re
 
 
 
 def cadastrar(request):
     if request.method == "GET":
         return render(request=request, template_name="cadastrar.html")
-    else:
-        usuario = nome = request.POST.get("nome")
-        email = request.POST.get("email")
-        senha = request.POST.get("senha")
-        if Usuario.objects.filter(email=email):
-            print(Usuario.objects.filter(email=email))
-            messages.error(request, "Email já cadastrado")
-            return redirect("cadastrar")
-        
-        Usuario.objects.create_user(email=email, nome=nome, password=senha)
-        messages.success(request, "Usuário cadastrado com sucesso")
-        return redirect('cadastrar')
+    elif request.method == "POST":
+        # Convertendo body em JSON da requisição em um dicionário
+        usuario = json.loads(request.body)
 
+        # Inicializando o dicionário de resposta
+        responseJson = {
+            "sucesso": True,
+            "senhasDiferentes": False,
+            "camposVazios": False,
+            "emailCadastrado": False,
+            "senhaInvalida": False,
+            "nomeInvalido": False
+        }
+        
+        # Executando validações
+        if not usuario['senha'] == usuario['senhaRepetida']:
+            responseJson.update({"sucesso": False, "senhasDiferentes": True})
+        
+        for chave in usuario:
+            if usuario[chave] == "":
+                responseJson.update({"sucesso": False, "camposVazios": True})
+        
+        if Usuario.objects.filter(email=usuario['email']):
+            responseJson.update({"sucesso": False, "emailCadastrado": True})
+
+        if not re.match(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", usuario['senha']):
+            responseJson.update({"sucesso": False, "senhaFraca": True})
+
+        if not re.match(r"^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$", usuario['nome']):
+            responseJson.update({"sucesso": False, "nomeInvalido": True})
+
+        # Se todas as validações passarem, o usuário é cadastrado e a mensagem é enviada. No front a tela será recarregada para aparecer a mensagem
+        if responseJson['sucesso']:
+            Usuario.objects.create_user(email=usuario['email'], nome=usuario['nome'], password=usuario['senha'])
+            messages.add_message(request, messages.SUCCESS, "Usuário cadastrado com sucesso")
+
+        return JsonResponse(responseJson)
         
--- static/js/cadastrar.js
+++ static/js/cadastrar.js
@@ -1,52 +1,63 @@
-// Selecionando elementos que serão verificados
-
-console.log("Cadastrar.js")
-const senha = document.querySelector("#senha")
-const senhaRepetida = document.querySelector("#senha-repetida")
-const form = document.querySelector("form")
-const mensagensErro = {
-    mensagemSenhas: document.querySelector("#mensagem-senhas"),
-    mensagemSenhaFraca: document.querySelector("#mensagem-senha-fraca"),
-    mensagemNome: document.querySelector("#mensagem-nome")
+// Função para pegar o token CSRF
+function getCSRFToken() {
+    return document.querySelector('input[name="csrfmiddlewaretoken"]').value;
 }
 
-const regexNome = /^[A-Za-zÀ-ÖØ-öø-ÿ\s]+$/;
-const regexSenha = /(?=^.{8,}$)((?=.*\d)(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/g
+const mensagensErro = {
+    mensagemEmail: document.querySelector("#mensagem-email"),
+    mensagemSenhas: document.querySelector("#mensagem-senhas"),
+    mensagemSenhaFraca: document.querySelector("#mensagem-senha-fraca"),
+    mensagemNome: document.querySelector("#mensagem-nome"),
+}
 
+async function enviarForm() {
+    // Pegando valores dos inputs e colocando em um objeto
+    let formBody = {
+        'email': document.querySelector("#email").value,
+        'nome': document.querySelector("#nome").value,
+        'senha': document.querySelector("#senha").value,
+        'senhaRepetida': document.querySelector("#senha-repetida").value
+    }
+    
+    // Enviando requisição POST para o servidor
+    await fetch("/cadastrar/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "X-CSRFToken": getCSRFToken()
+        },
+        body: JSON.stringify(formBody)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        if (!data["sucesso"]) {
+            // Caso alguma validação tenha falhado, as mensagem de erro correspondente será exibida
+            mensagensErro.mensagemEmail.hidden = data["emailCadastrado"] ? false : true
+            mensagensErro.mensagemNome.hidden = data["nomeInvalido"] ? false : true
+            mensagensErro.mensagemSenhas.hidden = data["senhasDiferentes"] ? false : true
+            mensagensErro.mensagemSenhaFraca.hidden = data["senhaFraca"] ? false : true
+        }
+        else {
+            // Recarregando a página caso o cadastro tenha sido bem sucedido. Escolhi por recarregar a pagina e não apenas mostrar a mensagem porque isso me parece mais intuitivo ao usuário
+            location.reload()
+        }
+    })
+}
 
-// Adicionando função para executrar quando o form for submetido. Caso alguma validação não passe a submissão não irá acontecer.
+// Adicionando evento para que quando o form for submetido a função "enviarForm()"  seja chamada
+const form = document.querySelector("form")
 form.addEventListener("submit", (e) => {
-    // verificando se a senhas são iguais
-    if (senha.value !== senhaRepetida.value) {
-        e.preventDefault()
-        mensagensErro.mensagemSenhas.hidden = false
-    }
-    else {
-        mensagensErro.mensagemSenhas.hidden = true
-    } 
-
-    // Verificando se a senha é fraca
-    if (!regexSenha.test(senha.value)) {
-        e.preventDefault()
-        mensagensErro.mensagemSenhaFraca.hidden = false
-    }
-    else {
-        mensagensErro.mensagemSenhaFraca.hidden = true
-    }
-
-    // Verificando se o nome é válido
-    if (!regexNome.test(nome.value)) {
-        e.preventDefault()
-        mensagensErro.mensagemNome.hidden = false
-    }
-    else {
-        mensagensErro.mensagemNome.hidden = true
-    }
-
+    e.preventDefault()
+    enviarForm()
 })
 
-// Função para alterar visibilidade da senha quando o icone for clicado
+
+
+// Função para alterar visibilidade da senha quando o ícone for clicado
 function alterarVisibilidadeSenha(olho) {
+    const senha = document.querySelector("#senha")
+    const senhaRepetida = document.querySelector("#senha-repetida")
     if (senha.type === "password") {
 
         senha.type = "text"
