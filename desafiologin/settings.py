2eaad47f0db7946dd019e93fe522e93bb29d54a1
Alberto Cesar <141800257+albertojbe@users.noreply.github.com>
2025-06-07 10:46:46 -0300

    Update settings.py

--- desafiologin/settings.py
+++ desafiologin/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['desafio-login-oc39.onrender.com']
 
 
 # Application definition
